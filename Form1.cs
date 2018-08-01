630bf8af44475ca1c3f16312fcc80724af46d12e
詹博丞 <36940888+peter2787@users.noreply.github.com>
2018-08-01 13:06:53 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Computer2._0
+{
+    public partial class Form1 : Form
+    {
+        Double value = 0;
+        String operation = "";
+        bool operation_pressed = false;
+        bool AfterEquals = false;
+     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if ((AfterEquals == true) && (result.Text != "0"))
+            {
+                result.Clear();
+                AfterEquals = false;
+            }
+
+            if ((result.Text == "0")||(operation_pressed))
+                result.Clear();
+
+            operation_pressed = false;
+            Button b = (Button)sender;
+            result.Text = result.Text + b.Text;
+        }
+
+        private void ClearErrorButton_Click(object sender, EventArgs e)
+        {
+            result.Text = "0";
+        }
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            result.Text = "0";
+            value = 0;
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            operation = b.Text;
+            value = Double.Parse(result.Text);
+            operation_pressed = true;
+            equation.Text = value + " " + operation;
+        }
+
+        private void Equals_Click(object sender, EventArgs e)
+        {
+            AfterEquals = true;
+            equation.Text = "";
+            switch(operation)
+            {
+                case "+":
+                    result.Text = (value + Double.Parse(result.Text)).ToString();
+                    break;
+
+                case "-":
+                    result.Text = (value - Double.Parse(result.Text)).ToString();
+                    break;
+
+                case "*":
+                    result.Text = (value * Double.Parse(result.Text)).ToString();
+                    break;
+
+                case "/":
+                    result.Text = (value / Double.Parse(result.Text)).ToString();
+                    break;
+
+                default:
+                    break;
+
+            }
+        }
+    }
+}
